c407534600199d0f447e306ee31b506365f4c50a
yeonuko <122000501+yeonuko@users.noreply.github.com>
2023-01-18 14:32:58 +0900

    Add files via upload

---
+++ linkfile/hanatour/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+
+
+    // ↓ 메인배너 img_slide slick
+
+    $(".move").slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        arrows: true
+    });
+
+
+}) //jQuery
